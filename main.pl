1f2c5510b65ed540bf88616fa4b60b0e9d896637
MaxKonovalov38 <max.kon.38r@gmail.com>
2020-11-11 14:41:43 +0800

    описана функция конвертир строки в хеш

--- main.pl
+++ main.pl
@@ -11,6 +11,7 @@ my $pi = 3.14159265358979323846;
 my $earth_radius = 6371.0;
 
 sub convert_js {
+	#
 	my $file = shift;
 
 	open(my $fh, '<', $file) or die "Couldn't open myinfo: $!\n";
@@ -20,11 +21,32 @@ sub convert_js {
 		$line =~ s/\}//;
 		$line =~ s/\{//;
 		$line =~ s/:/,/g;
-		print $line, "\n";
+		$line =~ s/ //g;
+		my %hash_data = arr_value($line);
+		fun_task();
 		last;
 	}
 
 	close($fh);
+}
+
+sub arr_value {
+	#
+	my $s = shift;
+	my @data = split( /,/, $s);
+	my %hash_val;
+	my $n = 0;
+
+	while ($n<scalar(@data)) {
+		$hash_val{$data[$n]} = $data[$n+1];
+		$n+=2;
+	}
+
+	return %hash_val;
+}
+
+sub fun_task {
+	my ($lat1, $lon1) = @_;
 }
 
 convert_js($js_file);
